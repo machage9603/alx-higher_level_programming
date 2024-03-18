0b385056b70771dfb8fc81be17654e2b1b3f4c1c
machage9603 <mikemachage@gmail.com>
2024-03-19 02:14:48 +0300

    list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def print_list_interger(my_list=[]):
-    for interger in my_list:
-        print('{:d}'.format(interger))
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print('{:d}'.format(integer))
