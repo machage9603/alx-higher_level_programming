d081b8bf995c767e8ec339bb4135d0da7b728b0f
machage9603 <mikemachage@gmail.com>
2024-07-06 12:35:55 +0300

    .#6: concatenate two arguments with is

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// import built-in module process
+const process = require('process');
+
+const firstArg = process.argv;
+const secondArg = process.argv;
+console.log(firstArg[2] + ' is ' + secondArg[3]);
