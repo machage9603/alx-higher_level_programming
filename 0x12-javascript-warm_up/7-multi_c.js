d19ae97478dcc74cce9b3d1e4c953ef19ac47c31
machage9603 <mikemachage@gmail.com>
2024-07-08 13:08:11 +0300

    print C is fun x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const process = require('process');
+const firstArg = process.argv[2];
+
+const x = parseInt(firstArg);
+
+//check if x is a number
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
