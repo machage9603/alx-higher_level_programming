e68457698f6db3df2d73cc960529e7cc45cfbd23
machage9603 <mikemachage@gmail.com>
2024-07-12 07:06:26 +0300

    #4: write class for rectage 4

--- 0x13-javascript_objects_scopes_closures/4-reactangle.js
+++ 0x13-javascript_objects_scopes_closures/4-reactangle.js
@@ -1,29 +1,33 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (h > 0 && w > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+      Object.create(null);
     }
   }
 
   print () {
+    if (!this.width || !this.height) {
+      return;
+    }
     for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
