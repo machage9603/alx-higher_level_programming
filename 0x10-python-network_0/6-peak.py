775faed47d74b999f58babc2d389181e093406c0
machage9603 <mikemachage@gmail.com>
2024-07-19 09:21:56 +0300

    #6: find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module finds the peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Prototype for this sorting"""
+    if not list_of_integers:
+        return None
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]
