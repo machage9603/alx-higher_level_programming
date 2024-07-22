1c51075d91628203b6dccdca88b5ef9a58ec841f
machage9603 <mikemachage@gmail.com>
2024-07-22 08:34:18 +0300

    #1: display response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Displays response header
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        print(response.info())
